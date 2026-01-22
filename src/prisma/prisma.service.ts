1c3682733465438adca0d3a680e4fd07b7878507
Sahilvashisth001 <Sahilvashisth001@gmail.com>
2026-01-22 14:04:53 +0530

    prisma setup

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    super({
+      adapter,
+      log:
+        process.env.NODE_ENV === 'development'
+          ? ['query', 'error', 'warn']
+          : ['error'],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Database connected Successfully');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('Database disconnected');
+  }
+
+  async cleanDatabase() {
+    if (process.env.NODE_ENV === 'production') {
+      throw new Error('Cannot clean database in production environment');
+    }
+    const model = Reflect.ownKeys(this).filter(
+      (key) => typeof key === 'string' && !key.startsWith('_'),
+    );
+
+    return Promise.all(
+      model.map((modelKey) => {
+        if (typeof modelKey === 'string') {
+          return this[modelKey].deleteMany();
+        }
+      }),
+    );
+  }
+}
